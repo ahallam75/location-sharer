2605cb680da691c5581f93692efa5e49910ef524
ahallam <alexhallam75@gmail.com>
2022-11-08 18:20:02 -0500

    UserItem, UserList, Avatar, and Card.

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
+import UserList from '../components/UserList'
+
 const Users = () => {
-  return <h2>Users Works!!!</h2>
+  const USERS = [
+    {
+      id: 'u1',
+      name: 'John Doe',
+      image: 'https://cdn.vuetifyjs.com/images/profiles/marcus.jpg',
+      places: 3
+    }
+  ];
+  return <UserList items={USERS} />
 };
 
 export default Users;
