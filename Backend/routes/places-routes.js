cf4d1763334378c4a42ad5ebb192e9d19e5c28b2
ahallam <alexhallam75@gmail.com>
2022-11-21 13:01:17 -0500

    Error handling

--- Backend/routes/places-routes.js
+++ Backend/routes/places-routes.js
@@ -13,17 +13,6 @@ const DUMMY_PLACES = [
     },
     address: '20 W 34th St, New York, NY 10001',
     creator: 'u1'
-  },
-  {
-    id: 'p2',
-    title: 'Willis Tower',
-    description: 'One of the most famous sky scrapers in the world!',
-    location: {
-      lat: 40.7484474,
-      lng: -73.9871516
-    },
-    address: 'Chicago, IL',
-    creator: 'u2'
   }
 ];
 
@@ -34,6 +23,12 @@ router.get('/:pid', (req, res, next) => {
     return p.id === placeId;
   });
 
+  if (!place) {
+    const error = new Error('Could not find a place for the provided id.');
+    error.code = 404;
+    throw error;
+  }
+
   res.json({ place }); // => { place } => { place: place }
 });
 
@@ -43,6 +38,12 @@ router.get('/user/:uid', (req, res, next) => {
   const place = DUMMY_PLACES.find(p => {
     return p.creator === userId;
   });
+
+  if (!place) {
+    const error = new Error('Could not find a place for the provided user id.');
+    error.code = 404;
+    return next(error);
+  }
 
   res.json({ place });
 });
